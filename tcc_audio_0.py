8c9f5dfde23c8d44a055140d2ced6abf3141f06e
André Moreira Souza <msouza.andre@hotmail.com>
2020-09-13 22:22:21 -0300

    more tests with librosa

--- tcc_audio_0.py
+++ tcc_audio_0.py
@@ -8,7 +8,8 @@ import librosa.display
 import numpy as np
 import os
 from pprint import pprint
-import subprocess
+from scipy.signal import butter, filtfilt
+# import subprocess
 
 # %% [markdown]
 # # Extracting raw audio from .mp4 files
@@ -22,26 +23,27 @@ print('Directory:', DATA_DIR)
 pprint(video_filenames)
 
 # %%
-for fname in video_filenames:
-    audio_fname = fname[:-4] + '.wav'
-    print(fname + " -> " + audio_fname)
-    p = subprocess.run([
-        '/home/andre/anaconda3/envs/audio/bin/ffmpeg', '-y', '-hide_banner',
-        '-loglevel', 'warning', '-i', DATA_DIR + fname, '-vn',
-        DATA_DIR + audio_fname
-    ],
-                       capture_output=True)
-    print('Output: ', end='')
-    pprint(p.stdout)
-    print('Err: ', end='')
-    pprint(p.stderr)
+# ? Removing audio conversion (librosa can load from .mp4)
+# for fname in video_filenames:
+#     audio_fname = fname[:-4] + '.wav'
+#     print(fname + " -> " + audio_fname)
+#     p = subprocess.run([
+#         '/home/andre/anaconda3/envs/audio/bin/ffmpeg', '-y', '-hide_banner',
+#         '-loglevel', 'warning', '-i', DATA_DIR + fname, '-vn',
+#         DATA_DIR + audio_fname
+#     ],
+#                        capture_output=True)
+#     print('Output: ', end='')
+#     pprint(p.stdout)
+#     print('Err: ', end='')
+#     pprint(p.stderr)
 
 # %% [markdown]
 # # Selecting a random audio from subset and experimenting with it
 
 # %%
 audio_filenames: list = [
-    fname for fname in os.listdir(DATA_DIR) if fname.endswith('.wav')
+    fname for fname in os.listdir(DATA_DIR) if fname.endswith('.mp4')
 ]
 print('Directory:', DATA_DIR)
 pprint(audio_filenames)
@@ -73,6 +75,44 @@ librosa.display.specshow(
 plt.draw()
 
 # %%
+# Zero Crossings
 zero_crossing = librosa.zero_crossings(x)
+pprint(zero_crossing)
+print("Zero crossing rate:", sum(zero_crossing) / len(zero_crossing))
+
+# %%
+# High-pass filter (?)
+
+# %%
+# Onset detection
+o_env = librosa.onset.onset_strength(x, sr=sr)
+times = librosa.frames_to_time(np.arange(len(o_env)), sr=sr)
+onset_frames = librosa.util.peak_pick(o_env, 3, 3, 3, 5, 0.3, 100)
+onset_detection = librosa.onset.onset_detect(x, sr=sr)
+
+# %%
+D = np.abs(librosa.stft(x))
+
+plt.figure(figsize=(15, 10))
+
+ax1 = plt.subplot(2, 1, 1)
+librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
+                         x_axis='time',
+                         y_axis='log')
+plt.title('Power spectrogram')
+
+plt.subplot(2, 1, 2, sharex=ax1)
+plt.plot(times, o_env, label='Onset strength')
+plt.vlines(times[onset_frames],
+           0,
+           o_env.max(),
+           color='r',
+           alpha=0.9,
+           linestyle='--',
+           label='Onsets')
+
+plt.axis('tight')
+plt.legend(frameon=True, framealpha=0.75)
+plt.draw()
 
 # %%
